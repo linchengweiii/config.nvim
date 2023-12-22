65961f7c7ce63ca39d59707cdf23fe70635d6987
linchengweiii <linchengweiii@gmail.com>
2023-12-22 13:06:53 +0800

    feat: add telescope git status keymapping

--- lua/core/keymapping.lua
+++ lua/core/keymapping.lua
@@ -33,6 +33,7 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
+vim.keymap.set('n', '<leader>gs', require('telescope.builtin').git_status, { desc = 'Search [G]it [S]tatus' })
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
